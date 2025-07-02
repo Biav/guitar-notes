128b7bcabca9e80df2de52627bb67392e142921f
Biav <beatrizvieirasilva.v@gmail.com>
2025-07-02 11:38:40 +0000

    feat: adding nuxt ui

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>
+    <UCard variant="subtle">
+      <!-- <div class="h-32 bg-gray-200 rounded" /> -->
+      <UCard variant="subtle">
+        <div class="h-32 w-32 bg-gray-200 rounded" />
+      </UCard>
+    </UCard>
+  </h1>
+</template>
+
+<script setup></script>
